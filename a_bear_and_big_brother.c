ddac9f6e1cf89e4208fdcefbb7ebb2d52703d443
IgorNascAlves <igor.nascimento.flipe@gmail.com>
2021-01-27 16:23:02 -0300

    solução matematica

--- a_bear_and_big_brother.c
+++ a_bear_and_big_brother.c
@@ -1,16 +1,18 @@
 //https://codeforces.com/problemset/problem/791/A
 
 #include <stdio.h>   
+#include <math.h>
+
+//3^x * a = 2^x * b
+//(3/2)^x = b/a
+//x = log3/2 b/a = log10 b/a / log10 3/2
 
 int main(){
-    int a=0, b=0, count=0;
-    scanf("%d", &a);
-    scanf("%d", &b);
-    while(a <= b){
-        a *= 3;
-        b *= 2;
-        count++;
-    }
-    printf("%d", count);
+    float a=0, b=0;
+    int count=0;
+    scanf("%f", &a);
+    scanf("%f", &b);
+    count = (int)((log10(b/a)/0.17609) + 1);
+    printf("%d\n", count);
     return 0;
 }
